4a80d560540d3bf705aba0020d99dcec43c3492a
mar <martorrijos@protonmail.com>
2025-04-29 16:19:33 +0200

    Initial commit

---
+++ src/level_1/ejercicio_1/Main.java
@@ -0,0 +1,24 @@
+package level_1.ejercicio_1;
+
+/*
+    SINGLETON
+
+    Crea una classe que repliqui el funcionament del comando 'Undo'.
+    Aquesta classe serà utilitzada per la classe Main, que permetrà introduir opcions per consola.
+
+    - La classe 'Undo' ha de guardar les últimes comandes introduïdes.
+    Ha de permetre afegir o eliminar comandes, així com llistar les últimes comandes introduïdes
+    (semblant a la comanda 'history' en Linux).
+
+    - La classe 'Undo' ha d'implementar imprescindiblement un patró Singleton.
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/level_2/ejercicio_1/Main.java
@@ -0,0 +1,11 @@
+package level_2.ejercicio_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
